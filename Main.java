c5a3dd87fd663e5fffb95060d6f22a1b520837e7
majd <killers1239333@gmail.com>
2018-05-30 22:48:13 +0300

    Main.java added

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x,y;
+        float r;
+        x=3;
+        y=10;
+        r=(float)y/x;
+        System.out.println(r);
+        System.out.println("the original file is also modified");
+
+    }
+}
